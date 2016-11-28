d11e647f434c763a4c6c0a2c4441643466b937e3
Ethan Xue <ethan.xue@gmail.com>
2016-11-28 01:04:35 -0500

    Update Bullet.cpp

--- Project_Shooter/Bullet.cpp
+++ Project_Shooter/Bullet.cpp
@@ -1,6 +1,7 @@
 #include "Bullet.h"
 
 Bullet::Bullet(int rowNum, int playerXpos, int playerYpos, int playerSize, double bulletSpeed) {
+  
   this->rowNum = rowNum;
   xpos = playerXpos + playerSize;
   ypos = playerYpos + playerSize / 2;
